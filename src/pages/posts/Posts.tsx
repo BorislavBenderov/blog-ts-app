265b39c5f4cb02969d150d9e0ef56dff9ca45dd5
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-07 16:31:36 +0200

    Render posts

--- src/pages/posts/Posts.tsx
+++ src/pages/posts/Posts.tsx
@@ -1,27 +1,24 @@
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import { useContext } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { SearchBar } from '../../components';
 import { Footer } from '../../components';
+import { IPost, PostContext, PostContextType } from '../../contexts/PostContext';
+import { PostCard } from '../../components/posts/PostCard';
 
 const theme = createTheme();
 
 export const Posts = () => {
+  const { posts } = useContext(PostContext) as PostContextType;
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <main>
-        {/* Hero unit */}
         <Box
           sx={{
             bgcolor: 'background.paper',
@@ -46,38 +43,15 @@ export const Posts = () => {
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
           <Grid container spacing={4} direction="column">
-            <Grid item xs={12} sm={6} md={4}>
-              <Card
-                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-              >
-                <CardMedia
-                  component="img"
-                  height='500px'
-                  image="https://source.unsplash.com/random"
-                  alt="random"
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe the
-                    content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Read</Button>
-                </CardActions>
-              </Card>
-            </Grid>
+            {posts.length > 0
+              ? posts.map((post: IPost) => <PostCard key={post.id} post={post} />)
+              : ''
+            }
           </Grid>
         </Container>
       </main>
-      {/* Footer */}
       <Footer />
-      {/* End footer */}
     </ThemeProvider>
   );
 }
