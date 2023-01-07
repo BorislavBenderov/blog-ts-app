58ab701a3a31a1fe1ae76a6f2ef7d05717cd2bfb
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-07 16:30:49 +0200

    Add Post Context

---
+++ src/contexts/PostContext.tsx
@@ -0,0 +1,40 @@
+import { collection, DocumentData, onSnapshot, orderBy, query, QuerySnapshot } from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
+import { database } from "../firebaseConfig";
+
+type ChildrenContextProviderProps = {
+    children: React.ReactNode
+}
+
+export type PostContextType = {
+    posts: IPost[];
+}
+
+export interface IPost {
+    title?: string,
+    description?: string,
+    id?: string
+}
+
+export const PostContext = createContext<PostContextType | null>(null);
+
+export const PostContextProvider = ({ children }: ChildrenContextProviderProps) => {
+    const [posts, setPosts] = useState<IPost[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const q = query(collection(database, 'posts'), orderBy("timestamp", 'desc'))
+        onSnapshot(q, (snapshot: QuerySnapshot<DocumentData>) => {
+            setPosts(snapshot.docs.map((item) => {
+                return { ...item.data(), id: item.id }
+            }));
+            setIsLoading(true);
+        });
+    }, []);
+    console.log(posts);
+    return (
+        <PostContext.Provider value={{ posts }}>
+            {children}
+        </PostContext.Provider>
+    );
+}
