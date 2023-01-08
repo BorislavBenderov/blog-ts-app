ae37de034a998070e668a77872e19ee30d130033
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-08 15:15:04 +0200

    Navigate to homepage after delete post

--- src/components/delete-post/DeletePost.jsx
+++ src/components/delete-post/DeletePost.jsx
@@ -1,16 +1,25 @@
 import { Button } from "@mui/material";
 import { deleteDoc, doc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 import { database } from "../../firebaseConfig";
 
 export const DeletePost = ({ postId }) => {
-    const onDeletePost = async () => {
+    const navigate = useNavigate();
+
+    const onDeletePost = () => {
         const confirmation = window.confirm('Are you sure you want to delete this post?');
 
         if (confirmation) {
-            deleteDoc(doc(database, 'posts', postId));
+            deleteDoc(doc(database, 'posts', postId))
+                .then(() => {
+                    navigate('/');
+                })
+                .catch((err) => {
+                    alert(err.message);
+                })
         }
     }
-    
+
     return (
         <Button size="small" onClick={onDeletePost}>Delete</Button>
     );
