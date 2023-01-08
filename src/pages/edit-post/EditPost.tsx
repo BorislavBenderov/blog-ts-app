054d7070bdc51d109ea191c95a0c9c6644feaeab
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-08 14:54:35 +0200

    Add EditPost page and possibility users to edit their posts

---
+++ src/pages/edit-post/EditPost.tsx
@@ -0,0 +1,137 @@
+import { useContext, useState } from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Footer } from '../../components';
+import { PostContext, PostContextType } from '../../contexts/PostContext';
+import { useNavigate, useParams } from 'react-router-dom';
+import { doc, updateDoc } from 'firebase/firestore';
+import { database } from '../../firebaseConfig';
+
+const theme = createTheme();
+
+export const EditPost = () => {
+    const { posts } = useContext(PostContext) as PostContextType;
+    const { postId } = useParams();
+    const navigate = useNavigate();
+    const currentPost = posts.find((post) => post.id === postId);
+
+    const [values, setValues] = useState({
+        title: currentPost?.title,
+        description: currentPost?.description,
+        imageUrl: currentPost?.imageUrl,
+        content: currentPost?.content
+    });
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValues((state) => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }))
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+
+        const title = formData.get('title');
+        const description = formData.get('description');
+        const imageUrl = formData.get('imageUrl');
+        const content = formData.get('content');
+
+        if (title === '' || description === '' || imageUrl === '' || content === '') {
+            alert('Please fill all the fields!');
+            return;
+        }
+
+        updateDoc(doc(database, `posts/${postId}`), {
+            title,
+            description,
+            imageUrl,
+            content
+        })
+            .then(() => {
+                navigate(`/posts/${postId}`);
+            })
+            .catch((err) => {
+                alert(err.message);
+            })
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h5">
+                        Edit Post
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="title"
+                            label="Title"
+                            name="title"
+                            value={values.title}
+                            onChange={changeHandler}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="description"
+                            label="Description"
+                            id="description"
+                            value={values.description}
+                            onChange={changeHandler}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="imageUrl"
+                            label="Image Url"
+                            id="imageUrl"
+                            value={values.imageUrl}
+                            onChange={changeHandler}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            multiline
+                            name="content"
+                            label="Content"
+                            id="content"
+                            value={values.content}
+                            onChange={changeHandler}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Edit
+                        </Button>
+                    </Box>
+                </Box>
+                <Footer />
+            </Container>
+        </ThemeProvider>
+    );
+}
