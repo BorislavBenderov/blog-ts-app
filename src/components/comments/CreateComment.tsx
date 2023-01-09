42f752c0474b64295e90af8fcc28fefec5139f32
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-09 09:03:50 +0200

    Add possibility users to create comments

---
+++ src/components/comments/CreateComment.tsx
@@ -0,0 +1,81 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Typography from '@mui/material/Typography';
+import { Box, Container } from "@mui/system";
+import { useContext, useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { database } from "../../firebaseConfig";
+import { AuthContext, IAuth } from "../../contexts/AuthContext";
+
+const theme = createTheme();
+
+export const CreateComment = ({ currentPost }: any) => {
+    const { loggedUser } = useContext(AuthContext) as IAuth;
+    const [input, setInput] = useState<string>('');
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (input === '') {
+            alert('Please enter a valid comment');
+            return;
+        }
+
+        addDoc(collection(database, 'comments'), {
+            text: input,
+            commentId: currentPost.id,
+            uid: loggedUser?.uid,
+            email: loggedUser?.email,
+            timestamp: serverTimestamp(),
+            likes: []
+        })
+            .then(() => {
+                setInput('');
+            })
+            .catch((err) => {
+                alert(err.message);
+            })
+    }
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h5">
+                        Create Comment
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            multiline
+                            name="comment"
+                            label="Comment"
+                            id="comment"
+                            value={input}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Add Comment
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+}
