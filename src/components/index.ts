68d0353bf5efe20ef23658cc81a96e2692e97317
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-09 09:46:03 +0200

    Export ProtectedRoutes component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export { Header } from './header/Header';
 export { SearchBar } from './search/SearchBar';
 export { Footer } from './footer/Footer';
+export { PostCard } from './posts/PostCard';
+export { Logout } from './logout/Logout';
+export { DeletePost } from './delete-post/DeletePost';
+export { ProtectedRoutes } from './protected-routes/ProtectedRoutes';
