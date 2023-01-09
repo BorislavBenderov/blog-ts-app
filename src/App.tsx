96271e062794d9ed7f9d526973cad067edd60f3f
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-09 09:05:17 +0200

    Use protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Header } from './components';
-import { Login, Register, Posts, PostDetails, EditPost, CreatePost, UserPosts } from './pages';
+import { Login, Register, Posts, PostDetails, EditPost, CreatePost, UserPosts, NotFound } from './pages';
 import { Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
 import { PostContextProvider } from './contexts/PostContext';
+import { ProtectedRoutes } from './components/protected-routes/ProtectedRoutes';
 
 function App() {
   return (
@@ -11,13 +12,16 @@ function App() {
         <div className="app">
           <Header />
           <Routes>
+            <Route path='*' element={<NotFound />} />
             <Route path='/' element={<Posts />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
             <Route path='/posts/:postId' element={<PostDetails />} />
-            <Route path='/edit/:postId' element={<EditPost />} />
-            <Route path='/create' element={<CreatePost />} />
-            <Route path='/my-posts' element={<UserPosts />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path='/edit/:postId' element={<EditPost />} />
+              <Route path='/create' element={<CreatePost />} />
+              <Route path='/my-posts' element={<UserPosts />} />
+            </Route>
           </Routes>
         </div>
       </PostContextProvider>
