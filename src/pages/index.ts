ac65f7a0ed94849ce7dd7f5c92df55ced52cee03
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-09 09:46:15 +0200

    Export all pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,7 @@ export { Login } from './login/Login';
 export { Register } from './register/Register';
 export { Posts } from './posts/Posts';
 export { PostDetails } from './post-details/PostDetails';
+export { EditPost } from './edit-post/EditPost';
+export { CreatePost } from './create-post/CreatePost';
+export { UserPosts } from './user-posts/UserPosts';
+export { NotFound } from './not-found/NotFound';
