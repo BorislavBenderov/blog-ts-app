2ea08cc16c51c2f81aeeab5489f7057010b1d3fc
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-04 08:58:05 +0200

    Change PostContextType

--- src/contexts/PostContext.tsx
+++ src/contexts/PostContext.tsx
@@ -1,44 +1,59 @@
-import { collection, DocumentData, onSnapshot, orderBy, query, QuerySnapshot } from "firebase/firestore";
+import {
+  collection,
+  DocumentData,
+  onSnapshot,
+  orderBy,
+  query,
+  QuerySnapshot,
+} from "firebase/firestore";
 import { createContext, useEffect, useState } from "react";
 import { database } from "../firebaseConfig";
 
 type ChildrenContextProviderProps = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 export type PostContextType = {
-    posts: IPost[];
-}
+  posts: IPost[];
+  isLoading: boolean;
+};
 
 export interface IPost {
-    title?: string,
-    description?: string,
-    id?: string,
-    imageUrl?: string,
-    content?: string,
-    ownerId?: string,
-    likes?: any
+  title?: string;
+  description?: string;
+  id?: string;
+  imageUrl?: string;
+  content?: string;
+  ownerId?: string;
+  likes?: any;
 }
 
 export const PostContext = createContext<PostContextType | null>(null);
 
-export const PostContextProvider = ({ children }: ChildrenContextProviderProps) => {
-    const [posts, setPosts] = useState<IPost[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
+export const PostContextProvider = ({
+  children,
+}: ChildrenContextProviderProps) => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const q = query(collection(database, 'posts'), orderBy("timestamp", 'desc'))
-        onSnapshot(q, (snapshot: QuerySnapshot<DocumentData>) => {
-            setPosts(snapshot.docs.map((item) => {
-                return { ...item.data(), id: item.id }
-            }));
-            setIsLoading(true);
-        });
-    }, []);
-
-    return (
-        <PostContext.Provider value={{ posts }}>
-            {children}
-        </PostContext.Provider>
+  useEffect(() => {
+    const q = query(
+      collection(database, "posts"),
+      orderBy("timestamp", "desc")
     );
-}
+    onSnapshot(q, (snapshot: QuerySnapshot<DocumentData>) => {
+      setPosts(
+        snapshot.docs.map((item) => {
+          return { ...item.data(), id: item.id };
+        })
+      );
+      setIsLoading(true);
+    });
+  }, []);
+
+  return (
+    <PostContext.Provider value={{ posts, isLoading }}>
+      {children}
+    </PostContext.Provider>
+  );
+};
