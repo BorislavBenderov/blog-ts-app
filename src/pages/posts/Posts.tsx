d95c37ffbb176d26746edaa04815c9c9d26a7d55
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-04 08:58:22 +0200

    Add loading state

--- src/pages/posts/Posts.tsx
+++ src/pages/posts/Posts.tsx
@@ -1,23 +1,30 @@
-import { useContext, useState } from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { SearchBar } from '../../components';
-import { Footer } from '../../components';
-import { IPost, PostContext, PostContextType } from '../../contexts/PostContext';
-import { PostCard } from '../../components/posts/PostCard';
+import { useContext, useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { SearchBar } from "../../components";
+import { Footer } from "../../components";
+import {
+  IPost,
+  PostContext,
+  PostContextType,
+} from "../../contexts/PostContext";
+import { PostCard } from "../../components/posts/PostCard";
 
 const theme = createTheme();
 
 export const Posts = () => {
-  const { posts } = useContext(PostContext) as PostContextType;
-  const [search, setSearch] = useState('');
+  const { posts, isLoading } = useContext(PostContext) as PostContextType;
+  const [search, setSearch] = useState("");
 
-  const searchedPosts = posts.filter(post => post?.title?.toLowerCase().includes(search.toLowerCase())
-    || post?.description?.toLowerCase().includes(search.toLowerCase()));
+  const searchedPosts = posts.filter(
+    (post) =>
+      post?.title?.toLowerCase().includes(search.toLowerCase()) ||
+      post?.description?.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <ThemeProvider theme={theme}>
@@ -25,7 +32,7 @@ export const Posts = () => {
       <main>
         <Box
           sx={{
-            bgcolor: 'background.paper',
+            bgcolor: "background.paper",
             pt: 8,
             pb: 6,
           }}
@@ -40,21 +47,28 @@ export const Posts = () => {
             >
               Blog Posts
             </Typography>
-            <Grid container justifyContent='center'>
+            <Grid container justifyContent="center">
               <SearchBar search={search} setSearch={setSearch} />
             </Grid>
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
           <Grid container spacing={4} direction="column">
-            {posts.length > 0
-              ? searchedPosts.map((post: IPost) => <PostCard key={post.id} post={post} />)
-              : ''
-            }
+            {isLoading ? (
+              posts.length > 0 ? (
+                searchedPosts.map((post: IPost) => (
+                  <PostCard key={post.id} post={post} />
+                ))
+              ) : (
+                <Typography>No Posts in Database!</Typography>
+              )
+            ) : (
+              <Typography sx={{ textAlign: "center" }}>Loading...</Typography>
+            )}
           </Grid>
         </Container>
       </main>
       <Footer />
     </ThemeProvider>
   );
-}
+};
